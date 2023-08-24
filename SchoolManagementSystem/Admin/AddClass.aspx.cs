e594fb209e58ec7763392bd3af9c825db69feb4c
redwildlion1 <trandafir.andrei03@yahoo.com>
2023-08-24 22:15:46 +0300

    design first admin page,can add and edit Classes

---
+++ SchoolManagementSystem/Admin/AddClass.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetClass();
+            }
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select Row_NUMBER() over(Order by (Select 1)) as [Sr.No], ClassId, ClassName from Class");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = fn.Fetch("Select * from Class where ClassName= '" + txtClass.Text.Trim() + "' ");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Class values('" + txtClass.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Succesfully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    txtClass.Text = string.Empty;
+                    GetClass();
+                }
+                else
+                {
+                    lblMsg.Text = "Entered Class already exists";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, System.Web.UI.WebControls.GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetClass();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetClass();
+        }
+
+        protected void GridView1_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+        }
+
+        protected void GridView1_RowUpdating(object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int cId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string ClassName = (row.FindControl("txtClassEdit") as TextBox).Text;
+                fn.Query("Update Class set ClassName = '" + ClassName + "' where ClassId = '" + cId + "' ");
+                lblMsg.Text = "Class Updated Succesfully!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetClass();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+    }
+}
