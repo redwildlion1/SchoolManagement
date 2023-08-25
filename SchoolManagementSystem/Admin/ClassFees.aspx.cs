ccacb16c7023ed5d4a5a84bdf448d692f9781f0c
redwildlion1 <trandafir.andrei03@yahoo.com>
2023-08-25 23:58:51 +0300

    added functionality for class fees

---
+++ SchoolManagementSystem/Admin/ClassFees.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Admin
+{
+    public partial class ClassFees : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/jquery-3.0.0.min.js",
+                    DebugPath = "~/Scripts/jquery-3.0.0.js",
+                    CdnPath = "https://code.jquery.com/jquery-3.0.0.min.js",
+                    CdnDebugPath = "https://code.jquery.com/jquery-3.0.0.js"
+                });
+
+                GetClass();
+                GetFees();
+            }
+        }
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select * from Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string classVal = ddlClass.SelectedItem.Text;
+                DataTable dt = fn.Fetch("Select * from Fees where ClassId='" + ddlClass.SelectedItem.Value + "'");
+                if (dt.Rows.Count == 0)
+                {
+                    string query = "Insert into Fees values('" + ddlClass.SelectedItem.Value + "','" + txtFeeAmounts.Text.Trim() + "')";
+                    fn.Query(query);
+                    lblMsg.Text = "Inserted Succesfully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    ddlClass.SelectedIndex = 0;
+                    txtFeeAmounts.Text = string.Empty;
+                    GetFees();
+                }
+                else
+                {
+                    lblMsg.Text = "Entered Fee already exists for <b>'" + classVal + "'<b>!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        private void GetFees()
+        {
+            DataTable dt = fn.Fetch("Select Row_NUMBER() over(Order by (Select 1)) as [Sr.No], f.FeesId, f.ClassId, c.ClassName," +
+                " f.FeesAmount from Fees f inner join Class c on c.ClassId = f.ClassId");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, System.Web.UI.WebControls.GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GetFees();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GetFees();
+        }
+
+        protected void GridView1_RowDeleting(object sender, System.Web.UI.WebControls.GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                int feesId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                fn.Query("Delete from Fees where FeesId = '" + feesId + "'");
+                lblMsg.Text = "Fee Delted Succesfully!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetFees();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GetFees();
+        }
+
+        protected void GridView1_RowUpdating(object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int feesId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                string feeAmt = (row.FindControl("TextBox1") as TextBox).Text;
+                fn.Query("Update Fees set FeesAmount = '" + feeAmt.Trim() + "' where FeesId = '" + feesId + "' ");
+                lblMsg.Text = "Fee Updated Succesfully!";
+                lblMsg.CssClass = "alert alert-success";
+                GridView1.EditIndex = -1;
+                GetFees();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
+        }
+    }
+}
