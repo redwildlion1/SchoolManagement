d6ef1b8a4d06065e3340dc660b7d9d1c24f04e13
redwildlion1 <trandafir.andrei03@yahoo.com>
2023-09-09 15:16:53 +0300

    funtionality for student attendance

---
+++ SchoolManagementSystem/Teacher/StudentAttendance.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static SchoolManagementSystem.Models.CommonFn;
+
+namespace SchoolManagementSystem.Teacher
+{
+    public partial class StudentAttendance : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition
+            {
+                Path = "~/Scripts/jquery-3.0.0.min.js",
+                DebugPath = "~/Scripts/jquery-3.0.0.js",
+                CdnPath = "https://code.jquery.com/jquery-3.0.0.min.js",
+                CdnDebugPath = "https://code.jquery.com/jquery-3.0.0.js"
+            });
+            if (!IsPostBack)
+            {
+                GetClass();
+                btnMarkAttendance.Visible = false;
+            }
+        }
+
+        protected void ddlClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string classId = ddlClass.SelectedValue;
+            DataTable dt = fn.Fetch("Select * from Subject where ClassId = '" + classId + "' ");
+            ddlSubject.DataSource = dt;
+            ddlSubject.DataTextField = "SubjectName";
+            ddlSubject.DataValueField = "SubjectId";
+            ddlSubject.DataBind();
+            ddlSubject.Items.Insert(0, "Select Subject");
+
+        }
+
+        private void GetClass()
+        {
+            DataTable dt = fn.Fetch("Select * from Class");
+            ddlClass.DataSource = dt;
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassId";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, "Select Class");
+        }
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            DataTable dt = fn.Fetch(@"Select StudentId, RollNo, Name,
+                                    Mobile from Student where ClassId = '" + ddlClass.SelectedValue + "' " +
+                                    "and ");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            if (dt.Rows.Count > 0)
+            {
+                btnMarkAttendance.Visible = true;
+            }
+        }
+
+        protected void btnMarkAttendance_Click(object sender, EventArgs e)
+        {
+            bool isTrue = false;
+            foreach (GridViewRow row in GridView1.Rows)
+            {
+                string rollNo = row.Cells[2].Text.Trim();
+                RadioButton rb1 = (row.Cells[0].FindControl("RadioButton1") as RadioButton);
+                RadioButton rb2 = (row.Cells[0].FindControl("RadioButton2") as RadioButton);
+                int status = 0;
+                if (rb1.Checked)
+                {
+                    status = 1;
+                }
+                else if (rb1.Checked)
+                {
+                    status = 0;
+                }
+
+                fn.Query(@"Insert into StudentAttendance values ('" + ddlClass.SelectedValue + "'," +
+                    "'" + ddlSubject.SelectedValue + "' , '" + rollNo + "','" + status + "'" +
+                         ",'" + DateTime.Now.ToString("yyyy/MM/dd") + "')");
+                isTrue = true;
+
+            }
+            if (isTrue)
+            {
+                lblMsg.Text = "Inserted Succesfully!";
+                lblMsg.CssClass = "alert alert-success";
+            }
+            else
+            {
+                lblMsg.Text = "Something went wrong :(!";
+                lblMsg.CssClass = "alert alert-warning";
+            }
+        }
+    }
+}
